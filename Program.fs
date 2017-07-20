c914f8a57cbb5b47e8fbf4c88613d281eaf146fc
Rémy Frèrebeau <remy@frerebeau.org>
2017-07-20 18:58:50 +0200

    initial commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+
+type NugetStats = HtmlProvider<"https://www.nuget.org/packages/FSharp.Data">
+
+
+[<EntryPoint>]
+let main argv =
+    let rawStats = NugetStats().Tables.``Version History``
+
+    rawStats.Rows |> Seq.iter (printfn "row = %A")
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,16 @@
+# A first workaround to use TypeProvider with F# and dotnet core (as of 20 th of July 2017)
+
+1. Install dotnet core preview 2
+https://www.microsoft.com/net/core/preview
+
+2. Install a recent F# 
+
+http://fsharp.org/use/windows/
+or 
+http://fsharp.org/use/linux/
+or
+http://fsharp.org/use/mac/
+
+3. Run
+
+``dotnet run``
